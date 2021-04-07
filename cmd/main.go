467dffac589fe2bc72b4c233a549683abaf004ea
Mikhail Shevelev <shevelevmo@sgaz.pro>
2021-04-07 16:57:29 +0500

    Influx

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/luckyshmo/gateway/config"
 	"github.com/luckyshmo/gateway/models"
 	"github.com/luckyshmo/gateway/pkg/repository"
+	"github.com/luckyshmo/gateway/pkg/repository/influx"
 	"github.com/luckyshmo/gateway/pkg/repository/kafkaQueue"
 	"github.com/luckyshmo/gateway/pkg/repository/pg"
 	"github.com/luckyshmo/gateway/pkg/service"
@@ -24,6 +25,8 @@ func main() {
 }
 
 func run() error {
+	influx.Influx()
+	return nil
 	// config
 	cfg := config.Get()
 
--- pkg/repository/influx/influx.go
+++ pkg/repository/influx/influx.go
@@ -1,36 +1,39 @@
 package influx
 
 import (
-	"context"
-	"fmt"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 )
+
+type influx struct {
+	writer api.WriteAPI
+}
 
 func Influx() {
 	// create new client with default option for server url authenticate by token
-	client := influxdb2.NewClient("http://localhost:9999", "my-token")
-	// user blocking write client for writes to desired bucket
-	writeAPI := client.WriteAPIBlocking("my-org", "my-bucket")
+	client := influxdb2.NewClient("http://localhost:8086", "HqVAEaFbGui5asqZYIMwDv4eNAhDl5wLraW4AanCbYX66hdq_zB_rj_SfpQ92haRgDTrX8YlT3aAOwOBwtYM3Q==")
+	// user async write client for writes to desired bucket
+	writeAPI := client.WriteAPI("myorg", "mybucket")
 	// create point using full params constructor
 	p := influxdb2.NewPoint("stat",
 		map[string]string{"unit": "temperature"},
 		map[string]interface{}{"avg": 24.5, "max": 45},
 		time.Now())
 	// write point immediately
-	writeAPI.WritePoint(context.Background(), p)
+	writeAPI.WritePoint(p)
 	// create point using fluent style
 	p = influxdb2.NewPointWithMeasurement("stat").
 		AddTag("unit", "temperature").
 		AddField("avg", 23.2).
 		AddField("max", 45).
 		SetTime(time.Now())
-	writeAPI.WritePoint(context.Background(), p)
+	writeAPI.WritePoint(p)
 
-	// Or write directly line protocol
-	line := fmt.Sprintf("stat,unit=temperature avg=%f,max=%f", 23.5, 45.0)
-	writeAPI.WriteRecord(context.Background(), line)
+	// // Or write directly line protocol
+	// line := fmt.Sprintf("stat,unit=temperature avg=%f,max=%f", 23.5, 45.0)
+	// writeAPI.WriteRecord(line)
 	// Ensures background processes finish
 	client.Close()
 }

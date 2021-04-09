540beb38ef75aac4b6029343b44cc3306338b176
Mikhail Shevelev <shevelevmo@sgaz.pro>
2021-04-09 12:18:27 +0500

    Propper Influx point generation

--- pkg/repository/influx/influx.go
+++ pkg/repository/influx/influx.go
@@ -1,39 +1,51 @@
 package influx
 
 import (
+	"context"
+	"fmt"
+	"math/rand"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api"
 )
 
-type influx struct {
-	writer api.WriteAPI
+type Influx struct {
+	writer api.WriteAPIBlocking
 }
 
-func Influx() {
+func NewInfluxWriter(address, token, org, bucket string) *Influx {
+	rand.Seed(42)
 	// create new client with default option for server url authenticate by token
-	client := influxdb2.NewClient("http://localhost:8086", "HqVAEaFbGui5asqZYIMwDv4eNAhDl5wLraW4AanCbYX66hdq_zB_rj_SfpQ92haRgDTrX8YlT3aAOwOBwtYM3Q==")
+	client := influxdb2.NewClient("http://localhost:8086", "sPdkEFcl8RuHVTD_HALyKWj0OWSkjhQdFb4pnzrYfSVrAunVW7JSzMr1mqkeyrXKt-BCG1VGabOMg6muiRVilg==")
 	// user async write client for writes to desired bucket
-	writeAPI := client.WriteAPI("myorg", "mybucket")
-	// create point using full params constructor
-	p := influxdb2.NewPoint("stat",
-		map[string]string{"unit": "temperature"},
-		map[string]interface{}{"avg": 24.5, "max": 45},
-		time.Now())
-	// write point immediately
-	writeAPI.WritePoint(p)
-	// create point using fluent style
-	p = influxdb2.NewPointWithMeasurement("stat").
-		AddTag("unit", "temperature").
-		AddField("avg", 23.2).
-		AddField("max", 45).
-		SetTime(time.Now())
-	writeAPI.WritePoint(p)
+	writeAPI := client.WriteAPIBlocking("myorg", "mybucket")
+	return &Influx{writeAPI}
+}
 
-	// // Or write directly line protocol
-	// line := fmt.Sprintf("stat,unit=temperature avg=%f,max=%f", 23.5, 45.0)
-	// writeAPI.WriteRecord(line)
-	// Ensures background processes finish
-	client.Close()
+func (wr *Influx) WriteData() {
+
+	for i := 0; i < 100; i++ {
+		// create data point
+		p := influxdb2.NewPoint(
+			"system",
+			map[string]string{
+				"id":       fmt.Sprintf("rack_%v", i%10),
+				"vendor":   "AWS",
+				"hostname": fmt.Sprintf("host_%v", i%100),
+			},
+			map[string]interface{}{
+				"temperature": rand.Float64() * 80.0,
+				"disk_free":   rand.Float64() * 1000.0,
+				"disk_total":  (i/10 + 1) * 1000000,
+				"mem_total":   (i/100 + 1) * 10000000,
+				"mem_free":    rand.Uint64(),
+			},
+			time.Now())
+		// write synchronously
+		err := wr.writer.WritePoint(context.Background(), p)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
